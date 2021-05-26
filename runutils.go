db97b0af686cc32247558160bbd6da84267f9d0d
caifh <caifh@txxy.com>
2021-05-26 11:07:48 +0800

    1.增加获取当前时间方法。

--- runutils.go
+++ runutils.go
@@ -1,6 +1,11 @@
 package runutils
 
-import "runtime"
+import (
+	"fmt"
+	"math"
+	"runtime"
+	"time"
+)
 
 // 获取正在运行的函数名
 func RunFuncName() string {
@@ -8,4 +13,10 @@ func RunFuncName() string {
 	runtime.Callers(2, pc)
 	f := runtime.FuncForPC(pc[0])
 	return f.Name()
+}
+
+func CurrentTime() string {
+	t := time.Now()
+	ms := int64(math.Mod(float64(t.UnixNano()), float64(time.Second))) / int64(time.Millisecond)
+	return fmt.Sprintf("%v-%v-%v %v:%v:%v:%v", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), ms)
 }
