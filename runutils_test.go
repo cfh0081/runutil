aeec40437f87e533e3b9ba9c71cdaa26e34640ab
caifh <caifh@txxy.com>
2021-06-03 15:29:56 +0800

    1.增加相应单元测试用例。

--- runutils_test.go
+++ runutils_test.go
@@ -7,8 +7,14 @@ import (
 )
 
 func TestRunFuncName(t *testing.T) {
-
 	expected := `github.com/cfh0081/runutils.TestRunFuncName`
 	name := RunFuncName()
 	assert.Equal(t, expected, name)
+}
+
+func TestRunFileAndLine(t *testing.T) {
+	// 需要和实际行号严格对应，否则会报错
+	expected := `runutils_test.go#18`
+	info := RunFileAndLine()
+	assert.Equal(t, expected, info)
 }
